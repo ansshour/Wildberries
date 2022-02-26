35ef9a413ca4ba064585b7e63aac62b16993e89b
ansshour <andrey.zhukov001@gmail.com>
2022-02-26 15:54:35 +0300

    third lesson

--- scripts/getgoods.js
+++ scripts/getgoods.js
@@ -1,18 +1,68 @@
 const GetGoods = () => {
+
     const nav__items = document.querySelectorAll(".navigation-link");
-    fetch("../db/db.json")
-        .then((res) => res.json())
-        .then((data) => {
-            nav__items.forEach((elem) => {
-                elem.addEventListener("click", (event) => {
-                    event.preventDefault;
-                    localStorage.setItem("goods", JSON.stringify(data));
-                    console.log(data);
-                });
-            })
-        })
 
 
+    const render = (good) => {
+        const goods_container = document.querySelector(".long-goods-list");
+        goods_container.innerHTML = "";
+        good.forEach((item) => {
+            const good_item = document.createElement("div");
+            good_item.classList.add("col-lg-3");
+            good_item.classList.add("col-sm-6");
+            good_item.innerHTML = `
+            <div class="goods-card">
+                <span class="label ${item.label ? null : 'd-none'}">${item.label}</span>
+                <img src="db/${item.img}" alt="image: ${item.name}" class="goods-image">
+                <h3 class="goods-title">${item.name}</h3>
+                <p class="goods-description">${item.description}</p>
+                <button class="button goods-card-btn add-to-cart" data-id="${item.id}">
+                    <span class="button-price">$${item.price}</span>
+                </button>
+            </div>
+            `
+            goods_container.append(good_item);
+        });
+    };
+
+    const GetData = (attrValue, itemInnerText) => {
+        fetch("../db/db.json")
+            .then((res) => res.json())
+            .then((data) => {
+                const dataFilter = attrValue ? data.filter((elem) => (elem[attrValue] == itemInnerText)) : data;
+                localStorage.setItem("goods", JSON.stringify(dataFilter));
+                if (window.location.pathname !== "/goods.html") {
+                    window.location.href = "/goods.html";
+                } else {
+                    render(dataFilter);
+                }
+
+            });
+    };
+
+
+    nav__items.forEach((item) => {
+        item.addEventListener("click", (event) => {
+            event.preventDefault();
+            const itemInnerText = item.textContent;
+            const attrValue = item.getAttribute("data-field");
+            GetData(attrValue, itemInnerText);
+        });
+    });
+
+    if (localStorage.getItem("goods") && (window.location.pathname === "/goods.html")) {
+        render(JSON.parse(localStorage.getItem("goods")));
+    }
+
+
+    const viewmoreBtn = document.querySelector(".more");
+    try {
+        viewmoreBtn.addEventListener("click", () => {
+            GetData();
+        });
+    } catch (er) {
+
+    }
 }
 
 GetGoods();
--- scripts/search.js
+++ scripts/search.js
@@ -4,12 +4,49 @@ const search = () => {
     let inputValue = "";
 
 
+    const GetData = (inputValue) => {
+        fetch("../db/db.json")
+            .then((res) => res.json())
+            .then((data) => {
+                const dataFilter = data.filter((item) => item.name.toLowerCase().includes(inputValue.toLowerCase()))
+                localStorage.setItem("goods", JSON.stringify(dataFilter));
+                if (window.location.pathname !== "/goods.html") {
+                    window.location.href = "/goods.html";
+                } else {
+                    render(dataFilter);
+                }
+
+            });
+    };
+
+    const render = (good) => {
+        const goods_container = document.querySelector(".long-goods-list");
+        goods_container.innerHTML = "";
+        good.forEach((item) => {
+            const good_item = document.createElement("div");
+            good_item.classList.add("col-lg-3");
+            good_item.classList.add("col-sm-6");
+            good_item.innerHTML = `
+            <div class="goods-card">
+                <span class="label ${item.label ? null : 'd-none'}">${item.label}</span>
+                <img src="db/${item.img}" alt="image: ${item.name}" class="goods-image">
+                <h3 class="goods-title">${item.name}</h3>
+                <p class="goods-description">${item.description}</p>
+                <button class="button goods-card-btn add-to-cart" data-id="${item.id}">
+                    <span class="button-price">$${item.price}</span>
+                </button>
+            </div>
+            `
+            goods_container.append(good_item);
+        });
+    };
+
     searchInput.addEventListener("input", (event) => {
         inputValue = event.target.value;
     });
 
     searchBtn.addEventListener("click", () => {
-        console.log(inputValue);
+        GetData(inputValue);
     });
 
 }
